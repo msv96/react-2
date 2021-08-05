5e8956419f4f817256c1eb53699b31ce18b34cdd
msv96 <msrini1100@gmail.com>
2021-08-05 18:25:11 +0530

    next needed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import useState from 'react';
 
 export default function App() {
+  const [like, setLike] = useState(0);
   return (
     <div className="App">
       <h1>hi</h1>
