abd73b9a92f8ad128649ebcd39acf266eb436b5d
msv96 <msrini1100@gmail.com>
2021-08-05 19:42:07 +0530

    click button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
-import useState from 'react';
+import "./App.css";
+import { useState } from "react";
 
 export default function App() {
   const [like, setLike] = useState(0);
   return (
     <div className="App">
-      <h1>hi</h1>
+      <button onClick={() => setLike(like - like)} className="reset">
+        Reset
+      </button>
+      <Btn></Btn>
+      <Btn></Btn>
+      <Btn></Btn>
+    </div>
+  );
+}
+
+function Btn() {
+  const [like, setLike] = useState(0);
+  return (
+    <div>
+      <div className="new">{like}</div>
+      <button onClick={() => setLike(like + 1)} className="main">
+        Click
+      </button>
     </div>
   );
 }
